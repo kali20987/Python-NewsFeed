167d1e9a05ae423a4d9f5be0e5db771956c83972
kali19 <khadijah.syed.ali@gmail.com>
2023-07-04 13:23:55 -0400

    errors fixed

--- app/models/Post.py
+++ app/models/Post.py
@@ -7,16 +7,24 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, fu
 from sqlalchemy.orm import relationship, column_property
 
 class Post(Base):
-  __tablename__ = 'posts'
-  id = Column(Integer, primary_key=True)
-  title = Column(String(100), nullable=False)
-  post_url = Column(String(100), nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-  user = relationship('User')
-  comments = relationship('Comment', cascade='all,delete')
-  votes = relationship('Vote', cascade='all,delete')
-  vote_count = column_property(
-  select([func.count(Vote.id)]).where(Vote.post_id == id)
-)
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    post_url = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user = relationship('User')
+    comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
+
+    #stmt = select(func.count(Vote.id)).where(Vote.id == "spongebob")
+    # stmt = select(func.count(Vote.id))
+    # print(stmt)
+
+    #print(select(func.count(Vote.id)).select_from(Vote))
+    #print(select(func.count(Vote.id)).where(Vote.post_id == id))
+
+    vote_count = column_property(
+      select(func.count(Vote.id)).where(Vote.post_id == id).scalar_subquery()
+    )
--- seeds.py
+++ seeds.py
@@ -14,7 +14,6 @@ db.add_all([
   User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
   User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
   User(username='djiri4', email='gmidgley4@weather.com', password='password123'),
-  User(username='alesmonde0', email='notvalid', password='password123')
 ])
 
 db.commit()
